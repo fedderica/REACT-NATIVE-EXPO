97575bbfae2e7444a881446c231d92d02be3dd86
fedderica <fedderica.bartoli@gmail.com>
2023-04-24 12:29:18 -0300

    confirma.PRIMERPLANOAPP

--- my-project/App.js
+++ my-project/App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+ import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg"
+ const { width, height } = Dimensions.get('window')
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Federica! </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.titulo}> Hola AYMI  </Text>
+      <Text style={styles.subTitle}> LOADING </Text>
+       <TextInput
+       placeholder="fedderica.bartoli@gmail.com"
+       style={styles.textInput} 
+       />
+        <TextInput
+       placeholder="password"
+       style={styles.textInput} 
+       />
+        <StatusBar style="auto" />
     </View>
   );
 }
@@ -13,8 +25,30 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1f1f1',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titulo: {
+    fontSize: 80,
+    color: "#34434D",
+    fontWeight: "bold",
+  },
+  subTitle: {
+    fontSize: 20,
+    color: "pink",
+  },
+
+textInput: {
+  borderWidth: 1,
+  paddingStart: 30,
+  padding: 10,
+  width: '80%',
+  height: 50,
+  marginTop: 20,
+  borderRadius: 30,
+  backgroundColor: '#fff',
+}
+
+
 });
