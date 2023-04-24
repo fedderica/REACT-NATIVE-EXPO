20b83cee1511c05aa1e7be5b2eb5a57cfb8d1127
fedderica <fedderica.bartoli@gmail.com>
2023-04-24 12:34:40 -0300

    elimina imagen por desición de estilos

--- my-project/App.js
+++ my-project/App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TextInput, Dimensions } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Dimensions, Image, ImageBackground } from 'react-native';
  import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg"
  const { width, height } = Dimensions.get('window')
+ import { Asset } from "expo-asset";
+
+
+
 
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.titulo}> Hola AYMI  </Text>
-      <Text style={styles.subTitle}> LOADING </Text>
+      <Text style={styles.titulo}>         I'M FEDERICA   </Text>
+      <Text style={styles.subTitle}>  THIS IS MY APP  </Text>
+      
+     
+      <ImageBackground
+      source={{
+        uri: Asset.fromModule(require("./img/fede.png")).uri,
+      }}
+      style={styles.image}
+    >
+      </ImageBackground>
+
        <TextInput
        placeholder="fedderica.bartoli@gmail.com"
        style={styles.textInput} 
        />
         <TextInput
-       placeholder="password"
+       placeholder="2216275798"
        style={styles.textInput} 
        />
         <StatusBar style="auto" />
@@ -25,13 +39,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#f1f1f1',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
   titulo: {
-    fontSize: 80,
-    color: "#34434D",
+    fontSize: 60,
+    color: "white",
     fontWeight: "bold",
   },
   subTitle: {
@@ -43,12 +57,19 @@ textInput: {
   borderWidth: 1,
   paddingStart: 30,
   padding: 10,
-  width: '80%',
+  width: '90%',
   height: 50,
   marginTop: 20,
   borderRadius: 30,
   backgroundColor: '#fff',
-}
+},
 
-
+image: {
+  width: 900,
+  height: 900,
+  flex: 1,
+  resizeMode: "cover",
+  justifyContent: "center",
+   opacity: 50
+},
 });
